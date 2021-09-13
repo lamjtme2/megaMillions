ab9e695b20adf032d27eb24b7fd6e520f47c2889
jack lam <lamjtme@hotmail.com>
2021-09-13 02:11:06 -0400

    repackaged jar to allow BC FIPS to work

---
+++ bin/go.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -classpath ./guessPerpetual_lib/*.jar -jar guessPerpetual.jar ../MegaMillions_7_30_2021.txt.ball.txt ../MegaMillions_7_30_2021.txt.megaball.txt 1000 25 ../MegaMillions_7_30_2021.txt
